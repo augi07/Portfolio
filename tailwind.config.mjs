c78893c522b03851f7636a08e1181f2aeba93c90
augi07 <102184778+augi07@users.noreply.github.com>
2024-10-18 10:44:39 +0200

    asd

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#5ab5b4',
+				'secondary': '#96afd0',
+				'accent': '#7a8bc3',
+				'background': '#f5fbfb',
+				'text': '#040a09',
+			}
+		},
 	},
 	plugins: [],
 }
