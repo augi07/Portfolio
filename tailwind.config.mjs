6ae08c474d47581ba2a69d45cbf831aa2e4335fd
A5TUT0 <noahlezama2000@gmail.com>
2024-10-18 11:52:17 +0200

    HERO scroll button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'primary': '#4aa5a4',
-				'secondary': '#2f496a',
-				'accent': '#3c4d86',
-				'background': '#04100f',
-				'text': '#e9f7f7',
-			}
-		},
-		fontFamily: {
-			'RobotoMono': 'Roboto Mono, monospace'
-		}
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4aa5a4",
+        secondary: "#2f496a",
+        accent: "#3c4d86",
+        background: "#000000",
+        text: "#e9f7f7",
+      },
+    },
+    fontFamily: {
+      RobotoMono: "Roboto Mono, monospace",
+    },
+  },
+  plugins: [],
+};
